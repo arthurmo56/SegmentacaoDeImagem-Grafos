2b1657c2c8c8beb27f4733d458afc00aeb8da651
arthurmo56 <arthurm.oliveira56@gmail.com>
2024-12-15 09:44:38 -0300

    segunda implementacao finalizada

--- artigo2/main.cpp
+++ artigo2/main.cpp
@@ -1,11 +1,11 @@
-
-
 #include "graph.hpp"
-#include "imageloader.hpp" // mesmo codigo do primeiro artigo
+#include "imageloader.hpp"
+#include "maxflow.hpp"
+#include "color_segments.hpp"
 
 int main()
 {
-    string filename = "images/teste.ppm";
+    string filename = "images/teste2.ppm";
     int width, height;
 
     try
@@ -13,38 +13,54 @@ int main()
         // Carregar a imagem e obter a matriz de pixels
         auto image = loadPPM(filename, width, height);
 
-        double sigma = 50.0;
+        double sigma = 10.0;
 
-        vector<double> objProb(width * height, 0.7); // Probabilidade inicial de ser objeto
-        vector<double> bgProb(width * height, 0.3);  // Probabilidade inicial de ser fundo
+        vector<double> objProb(width * height, 0.5); // Probabilidade inicial de ser objeto
+        vector<double> bgProb(width * height, 0.5);  // Probabilidade inicial de ser fundo
 
         vector<bool> isSeedObject(width * height, false);
         vector<bool> isSeedBackground(width * height, false);
         // Exemplo: marque alguns pixels manualmente como seeds
-        isSeedObject[2] = true;    
-        isSeedBackground[8] = true; 
+        // Função para verificar proximidade de uma cor
+
+        isSeedObject[1] = true;
+        objProb[1] = 0.99; // Alta probabilidade de objeto
+        bgProb[1] = 0.01;
+
+        isSeedBackground[15] = true;
+        objProb[15] = 0.01; // Alta probabilidade de fundo
+        bgProb[15] = 0.99;
 
         // Criar o grafo a partir da imagem
         Graph graph(image, width, height, sigma, objProb, bgProb);
 
-        // Adicionar restrições duras para seeds manuais
+        // Configurar os t-links
         for (int i = 0; i < width * height; ++i)
         {
+            graph.tLinkSource[i] = -log(objProb[i]);
+            graph.tLinkSink[i] = -log(bgProb[i]);
+
             if (isSeedObject[i])
             {
-                graph.tLinkSource[i] = INFINITY;
-                graph.tLinkSink[i] = 0;
+                graph.tLinkSource[i] = INFINITY; // Conecta ao source
+                graph.tLinkSink[i] = 0;          // Não conecta ao sink
             }
             if (isSeedBackground[i])
             {
-                graph.tLinkSource[i] = 0;
-                graph.tLinkSink[i] = INFINITY;
+                graph.tLinkSource[i] = 0;      // Não conecta ao source
+                graph.tLinkSink[i] = INFINITY; // Conecta ao sink
             }
         }
 
-        // Exibir o grafo para verificação
-        graph.printGraph();
+        // Calcular o fluxo máximo e obter o corte mínimo
+        double maxFlowValue = graph.maxFlow();
+        cout << "Fluxo máximo: " << maxFlowValue << endl;
 
+        vector<bool> segmentation = graph.segment();
+        cout << "Segmentação concluída. Tamanho: " << segmentation.size() << endl;
+
+        vector<vector<Pixel>> coloredImage = ImageSegmentColorizer::colorizeSegmentation(image, segmentation, width, height);
+        ImageSegmentColorizer::savePPM("images/saida.ppm", coloredImage, width, height);
     }
     catch (const exception &e)
     {
